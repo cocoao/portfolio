33dd3647fef83db0f1d09ef35a37c556b918d212
cocoao <cocoaooo212@gmail.com>
2021-03-09 11:17:48 +0900

    210308 포트폴리오 메인페이지 레이아웃 작업

---
+++ na_pf/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NA_HYEJEONG</title>
+  <!-- font awesome font link -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- css link -->
+  <link rel="stylesheet" href="/na_pf/css/reset.css">
+  <link rel="stylesheet" href="/na_pf/css/style.css">
+
+  <!-- jquery link -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="/na_pf/js/custom.js"></script>
+</head>
+<body>
+  <div class="wrap">
+    <div class="slideBox">
+    </div>
+    <div class="txtBoxes">
+      <div class="txtBox">
+        <div class="contents clear">
+          <div class="indexTxt">
+            <p>Information of me</p>
+            <hr>
+            <h3>INTRO</h3>
+          </div>
+          <div class="indexTxt">
+            <p>History of me</p>
+            <hr>
+            <h3>CAREER</h3>
+          </div>
+          <div class="indexTxt">
+            <p>My ability</p>
+            <hr>
+            <h3>SKILL</h3>
+          </div>
+          <div class="indexTxt">
+            <p>Contact me</p>
+            <hr>
+            <h3>CONTACT</h3>
+          </div>
+        </div>
+        <div class="copy">
+          <p>COPYRIGHTⓒ 2021 BY NA</p>
+        </div>
+      </div>
+      <div class="nav">
+        <ul>
+        <li><a href="#">
+            <p>MAIN</p>
+            <span class="hover">
+              <i class="fa fa-angle-double-right"></i><hr>
+            </span>
+          </a></li>
+          <li><a href="#">
+            <p>ABOUT ME</p>
+            <span class="hover">
+              <i class="fa fa-angle-double-right"></i><hr>
+            </span>
+          </a></li>
+          <li><a href="#">
+            <p>PUBLISHING</p>
+            <span class="hover">
+              <i class="fa fa-angle-double-right"></i><hr>
+            </span>
+          </a></li>
+          <li><a href="#">
+            <p>DESIGN</p>
+            <span class="hover">
+              <i class="fa fa-angle-double-right"></i><hr>
+            </span>
+          </a></li>
+          <li><a href="#">
+            <p>MORE WORKS</p>
+            <span class="hover">
+              <i class="fa fa-angle-double-right"></i><hr>
+            </span>
+          </a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
